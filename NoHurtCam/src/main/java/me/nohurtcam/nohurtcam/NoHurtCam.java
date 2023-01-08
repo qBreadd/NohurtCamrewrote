5bacc385fcb56e3a8233201232a50f337438b6c9
Breadd69 <122205414+Breadd69@users.noreply.github.com>
2023-01-08 11:48:14 +0100

    Source code

---
+++ NoHurtCam/src/main/java/me/nohurtcam/nohurtcam/NoHurtCam.java
@@ -0,0 +1,35 @@
+package me.nohurtcam.nohurtcam;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.options.KeyBinding;
+import net.minecraft.text.Text;
+import org.lwjgl.glfw.GLFW;
+
+public class NoHurtCam implements ModInitializer {
+    public static boolean toggledOn = false;
+    MinecraftClient client = MinecraftClient.getInstance();
+    @Override
+    public void onInitialize() {
+         KeyBinding k = KeyBindingHelper.registerKeyBinding(new KeyBinding("key.noHurtCam.toggle",
+                 GLFW.GLFW_KEY_F8,
+                "category.noHurtCam"));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (k.wasPressed()) {
+                if (toggledOn) {
+                    toggledOn = false;
+                    client.player.sendMessage(Text.of("§8[§aNoHurtCam§8] §aEnabled§7 Hurtcam"), false);
+                }
+                else {
+                    toggledOn = true;
+                    client.player.sendMessage(Text.of("§8[§aNoHurtCam§8] §cDisabled§7 Hurtcam"), false);
+                }
+
+
+            }
+        });
+    }
+}
